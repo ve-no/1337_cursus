0b914930ebf80b5167e02a0e983088ed75944775
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-11-19 20:58:43 +0100

    just a commit

--- dddddd/Level 2/print_bits/print_bits.c
+++ dddddd/Level 2/print_bits/print_bits.c
@@ -1,12 +1,18 @@
 #include <unistd.h>
 
+void putcvvcv(char c)
+{
+	write(1, &c, 1);
+}
+
 void print_bits(unsigned char octet)
 {
-	int	i = 8;
+	int	i = 128;
 	unsigned char 	bit;
 
 	while (i--)
 	{
+
 		bit = (octet >> i & 1) + '0';
 		write(1, &bit, 1);
 	}
