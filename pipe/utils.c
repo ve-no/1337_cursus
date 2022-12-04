253a1b0cc2c6922cfd70d746c0db19eb04491d50
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-12-04 10:04:27 +0100

    just a commit

---
+++ pipe/utils.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-bako <ael-bako@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 16:54:20 by ael-bako          #+#    #+#             */
+/*   Updated: 2022/12/04 10:01:01 by ael-bako         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*path_joi(char *path, char *bin)
+{
+	char	*joined;
+	int		i;
+	int		j;
+
+	joined = malloc(sizeof(char) * (str_ichr(path, 0) + str_ichr(bin, 0) + 2));
+	i = 0;
+	j = 0;
+	while (path[j])
+		joined[i++] = path[j++];
+	joined[i++] = '/';
+	j = 0;
+	while (bin[j])
+		joined[i++] = bin[j++];
+	joined[i] = 0;
+	return (joined);
+}
+
+int	str_ncmp(char *str1, char *str2, int n)
+{
+	while (--n > 0 && *str1 && *str2 && *str1 == *str2)
+	{
+		str1++;
+		str2++;
+	}
+	return (*str2 - *str1);
+}
+
+int	str_ichr(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != c)
+		i++;
+	if (str[i] == c)
+		return (i);
+	return (-1);
+}
+
+char	*str_ndup(char *str, unsigned int n)
+{
+	char				*duped;
+	unsigned int		i;
+
+	i = 0;
+	duped = malloc(sizeof(char) * (n + 1));
+	while (i < n)
+		duped[i++] = *str++;
+	duped[n] = 0;
+	return (duped);
+}
+
+char	**str_split(char *str, char sep)
+{
+	char	**tab;
+	int		count;
+	int		i;
+	int		j;
+
+	count = 0;
+	j = -1;
+	while (str[++j])
+		if ((str[j] == sep && str[j + 1] != sep)
+			|| (str[j] != sep && str[j + 1] == '\0'))
+			count++;
+	tab = malloc(sizeof(char *) * (count + 1));
+	i = 0;
+	while (i < count)
+	{
+		j = 0;
+		while (*str && *str == sep)
+			str++;
+		while (str[j] && str[j] != sep)
+			j++;
+		tab[i++] = str_ndup(str, j);
+		str = str + j;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
