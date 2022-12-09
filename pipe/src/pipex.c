d08fe3890c39f1de2b13f004f5e579e9bbbffaba
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-12-09 16:22:22 +0100

    just a commit

--- pipe/src/pipex.c
+++ pipe/src/pipex.c
@@ -6,97 +6,11 @@
 /*   By: ael-bako <ael-bako@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 16:54:00 by ael-bako          #+#    #+#             */
-/*   Updated: 2022/12/04 18:18:40 by ael-bako         ###   ########.fr       */
+/*   Updated: 2022/12/05 09:11:30 by ael-bako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
-
-int	ft_open(char *filename, int mode)
-{
-	if (mode == INFILE)
-	{
-		if (access(filename, F_OK))
-		{
-			write(STDERR, "pipex: ", 7);
-			write(STDERR, filename, str_ichr(filename, 0));
-			write(STDERR, ": No such file or directory\n", 28);
-			return (STDIN);
-		}
-		return (open(filename, O_RDONLY));
-	}
-	else
-		return (open(filename, O_CREAT | O_RDWR | O_TRUNC, 0644));
-}
-
-char	*get_path(char *cmd, char **env)
-{
-	char	*path;
-	char	*dir;
-	char	*bin;
-	int		i;
-
-	i = 0;
-	while (env[i] && str_ncmp(env[i], "PATH=", 5))
-		i++;
-	if (!env[i])
-		return (cmd);
-	path = env[i] + 5;
-	while (path && str_ichr(path, ':') > -1)
-	{
-		dir = str_ndup(path, str_ichr(path, ':'));
-		bin = join_path(dir, cmd);
-		free(dir);
-		if (access(bin, F_OK) == 0)
-			return (bin);
-		free(bin);
-		path += str_ichr(path, ':') + 1;
-	}
-	return (cmd);
-}
-
-void	exec(char *cmd, char **env)
-{
-	char	**args;
-	char	*path;
-
-	args = ft_split(cmd, ' ');
-	if (str_ichr(args[0], '/') > -1)
-		path = args[0];
-	else
-		path = get_path(args[0], env);
-	execve(path, args, env);
-	write(STDERR, "pipex: ", 7);
-	write(STDERR, cmd, str_ichr(cmd, 0));
-	write(STDERR, ": command not found\n", 20);
-	exit(127);
-}
-
-void	redir(char *cmd, char **env, int fdin)
-{
-	pid_t	pid;
-	int		pipefd[2];
-
-	pipe(pipefd);
-	pid = fork();
-	if (pid < 0)
-		return (perror("Fork"));
-	if (pid)
-	{
-		close(pipefd[1]);
-		dup2(pipefd[0], STDIN);
-		waitpid(pid, NULL, 0);
-	}
-	else
-	{
-		close(pipefd[0]);
-		dup2(pipefd[1], STDOUT);
-		if (fdin == STDIN)
-			exit(1);
-		else
-			exec(cmd, env);
-	}
-}
 
 int	main(int ac, char **av, char **env)
 {
